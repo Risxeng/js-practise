ea9f7987804654c447175f475c453d651e16348e
Rishav <rishavofficial0709@gmail.com>
2025-06-07 10:40:00 +0000

    Container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+// node 01_basics/01_variables.js
+const accountId = 7777777
+let accountEmai = "rishav@google.com"
+var accountpassword = "12354"
+accountcity = "patna"
+let accountstate
+
+// accountID =2
+accountEmai = "efwwf@ss.com"
+accountpassword = "5345436"
+accountcity = "Bengaluru"
+
+console.log(accountId);
+console.table([accountId, accountEmai, accountpassword, accountcity, accountstate])
+/*prefer not to use var becuz of issue in block scope and functional scope */
