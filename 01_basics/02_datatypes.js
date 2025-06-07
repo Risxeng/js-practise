2e61a10b6df2a6a2af5f55688b6e5ba1fb706a23
Rishav <rishavofficial0709@gmail.com>
2025-06-07 11:22:13 +0000

    Container config done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,15 @@
+"use strict"; //treat all js code  as newer version 
+console.log(3+3);
+let name = "Rishav"
+let state
+
+//number => 2^53
+// big int
+//string => ""
+//boolean
+//null => standalone valeue
+//undefined
+//symbol = > unique
+//object
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
