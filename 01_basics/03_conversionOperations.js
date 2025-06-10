f780c74a1fad50125fcce43b12222f3a501ed50a
Rishav <rishavofficial0709@gmail.com>
2025-06-10 12:48:43 +0000

    Container config done

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,5 +1,27 @@
 let score = 33
 console.log(typeof score);
+console.log(typeof (score));
 
 let valueInNumber = Number(score)
 console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1 , false => 0
+
+let isLoggedIn = "rishav"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+// 1 => true; 0 => false
+//"" => false
+//"rishav" => true
+
+let somenumber = 55
+let stringNumber = String(somenumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
