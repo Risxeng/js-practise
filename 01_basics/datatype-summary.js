e6efedd75d9f7940974e7ddf0da715d1e31dd84b
Rishav <rishavofficial0709@gmail.com>
2025-06-10 14:06:20 +0000

    change in location

---
+++ 01_basics/datatype-summary.js
@@ -0,0 +1,24 @@
+//primitive
+//7types : string,number,boolean,null,undefined,symbol,BigInt
+//js is dynamically typed
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id == anotherId);
+const Name = null
+
+
+//Refernce (non-primitive) : Array ,Objects, Functions
+
+const heros = ["shaktiman" , "naagraj", "doga"]
+let myobj = {
+    name:"hitesh",
+    aage: 22,
+}
+const myfunction = function(){
+    console.log("Hello World"); 
+}
+// console.log(typeof Name); => this is unique point this lecture
+
+
+
