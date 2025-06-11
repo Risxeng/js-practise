ab28894d598c7446794a2dc0f5870bdfcacc5731
Rishav <rishavofficial0709@gmail.com>
2025-06-11 08:55:14 +0000

    Container config done

--- 01_basics/datatype-summary.js
+++ 01_basics/datatype-summary.js
@@ -4,7 +4,7 @@
 
 const id = Symbol('123')
 const anotherId = Symbol('123')
-console.log(id == anotherId);
+// console.log(id == anotherId);
 const Name = null
 
 
@@ -15,10 +15,34 @@ let myobj = {
     name:"hitesh",
     aage: 22,
 }
-const myfunction = function(){
-    console.log("Hello World"); 
-}
+// const myfunction = function(){
+//     console.log("Hello World"); 
+// }
 // console.log(typeof Name); => this is unique point this lecture
 
 
 
+//**********************************************************************************
+
+
+//stack(primitive) , Heap(Non-Primitive)
+
+
+//stack
+// let myYoutubename = "rishav0709"
+// let anothername = myYoutubename
+// anothername = "chai aur code"
+// console.log(myYoutubename);
+// console.log(anothername);
+
+
+//Heap
+let userone = {
+    email: "user@google.com",
+    UPI: "user@ybl"
+}
+let userTwo = userone
+userTwo.email = "rishav@oole.com"
+
+console.log(userone.email);
+console.log(userTwo.email);
