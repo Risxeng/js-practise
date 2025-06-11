9c82fd04eb6e2547bac006ae6fe07f85c1f0604c
Rishav <rishavofficial0709@gmail.com>
2025-06-11 10:04:16 +0000

    Container config done

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,40 @@
+const name = "rishav"
+const repoCount = 2
+
+//console.log(`Hello my name is ${name} and my rpo count is ${repoCount}`);
+
+const gameName = new String('gum-nam-x-agent')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__); //=> this give result ({}) hence this shows it is a object prototype
+
+// console.log(gameName[0]);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName)
+
+// const newString= gameName.substring(0,4)
+// console.log(newString);
+
+// const anotherString = gameName.slice(-10,2)
+// console.log(anotherString);
+// const newStringOne = "   hitesh   "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url = "https://rishav.com.rishav%20nitian"
+console.log(url.replace('%20','-'));
+
+console.log(url.includes('rishu'));
+console.log(gameName.split('-'));
+
+
+
+
+
+
+
+
+
+
+
