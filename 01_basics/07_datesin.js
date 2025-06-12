295c5d1d6d89b751f876f009b0deadbaac628a55
Rishav <rishavofficial0709@gmail.com>
2025-06-12 16:34:03 +0000

    Container config done

---
+++ 01_basics/07_datesin.js
@@ -0,0 +1,29 @@
+let mydate = new Date()
+// console.log(mydate.toString());
+// console.log(mydate.toDateString());
+// console.log(mydate.toLocaleString());
+// console.log(typeof mydate);
+
+// let myCreateDate = new Date(2022 , 0 , 15)
+// let myCreateDate = new Date(2022 , 0 , 15 , 5 , 3)
+// let myCreateDate = new Date("2023-01-15")
+let myCreateDate = new Date("01-12-2022")
+// console.log(myCreateDate.toLocaleString());
+
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreateDate.getTime());
+// console.log(Math.floor(Date.now()/1000))
+
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());
+console.log(newDate.getMilliseconds());
+
+
+
+
+
