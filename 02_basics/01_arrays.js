852abd7fd233368690f9c11c6989bcba6287f6fe
Rishav <rishavofficial0709@gmail.com>
2025-06-13 10:21:29 +0000

    Container config done

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,3 +1,37 @@
 const myArr = [0,1,3,4,6,9]
-const myArr2 = new Array(1,2,5,6,7,9)
-console.log(myArr[3]);
+// const myArr2 = new Array(1,2,5,6,7,9)
+// console.log(myArr[3]);
+
+
+//array methods
+// myArr.push(7)
+// console.log(myArr);
+
+// myArr.pop()
+// console.log(myArr);
+
+// myArr.unshift(7)
+// myArr.shift()
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(9));
+
+// const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log(typeof newArr);
+// console.log(newArr);
+
+console.log("A",myArr);
+
+const myn1 = myArr.slice(1,5)
+console.log(myn1);
+console.log("B",myArr);
+
+const myn2 = myArr.splice(1,3)
+console.log(myn2);
+console.log("c",myArr);
+
+
+
+
