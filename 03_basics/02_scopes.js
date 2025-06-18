16f3eb2c89104c3a900dffe77cbdb898dba5768e
Rishav <rishavofficial0709@gmail.com>
2025-06-18 13:01:50 +0000

    Container config done

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -4,11 +4,51 @@ if(true){
     let a = 10
     const b = 20
     // var c = 30
-    console.log("INNER:", a);
+    // console.log("INNER:", a);
 
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
 
+
+function one(){
+    const username = "Rishav"
+    
+    function two() {
+        const website = "youtube"
+        // console.log(username);
+        
+    }
+    // console.log(website);
+    two()
+    console.log(username);
+    
+}
+// one()
+
+
+if(true){
+    const username = "Rishav "
+    if(username === "Rishav "){
+        const website = "youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+// *****************************intersecting ********************************
+console.log(addone(4))
+
+function addone(num){
+    return num + 1
+}
+
+
+
+const addtwo = function(num){
+    return num + 2
+}
+addtwo(9)
+console.log(addtwo(9))
