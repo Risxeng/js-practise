a6732441b460fb0600c216d9bfa3e5fc016dfa0e
Rishav <rishavofficial0709@gmail.com>
2025-06-20 10:01:08 +0000

    Container config done

---
+++ 04_controlflow/switch.js
@@ -0,0 +1,1 @@
+//switch => break , continue
---
+++ 04_controlflow/truthy.js
@@ -0,0 +1,8 @@
+// falsy value => false,0,-0,bigint,0n,"", null, undefined,NaN
+// truthy value => "0",'false'," ",[],{}, function(){}
+
+const emptyobj = {}
+
+if(Object.keys(emptyobj).length === 0) {
+    console.log("object is empty");
+}
