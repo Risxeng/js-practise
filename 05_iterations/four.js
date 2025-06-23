457181c129b452188cb823356fbc328d24f75776
Rishav <rishavofficial0709@gmail.com>
2025-06-23 08:50:55 +0000

    Container config done

---
+++ 05_iterations/four.js
@@ -0,0 +1,16 @@
+const myobj = {
+    js : 'javascript',
+    cpp : 'c++',
+    rb : 'ruby',
+}
+for (const key in myobj) {
+    //console.log(`${key} shortcut is for ${myobj[key]}`);
+}
+
+const programming = ["js","rb","py","java","cpp"]
+
+for (const key in programming) {
+    // console.log(programming[key]);   
+}
+
+
