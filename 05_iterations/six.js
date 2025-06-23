e344e6f6f6a8ae68245c2a026a8029546f4d55ce
Rishav <rishavofficial0709@gmail.com>
2025-06-23 11:03:40 +0000

    Container config done

---
+++ 05_iterations/six.js
@@ -0,0 +1,18 @@
+const coding = ["javascript","cpp","python","ruby","java"]
+
+// const values = coding.forEach((item) => {
+//     console.log(item);
+// })
+// console.log(values);
+
+const mynums = [1,2,3,4,5,6,7,8,9,10]
+// const newnums =mynums.filter( (num) =>  num > 4)
+// console.log(newnums);
+
+// const newnums = []
+// mynums.forEach((num) => {
+//     if (num > 2) {
+//         newnums.push(num)
+//     }
+// } )
+// console.log(newnums);
